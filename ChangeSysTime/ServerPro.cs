18d78b4c0387031c36ba26f7ec8964f55f6c95e6
jasongrass <ljunjiee@qq.com>
2016-05-27 21:41:24 +0800

    新增管理员权限检查

--- ChangeSysTime/ServerPro.cs
+++ ChangeSysTime/ServerPro.cs
@@ -19,34 +19,22 @@ namespace ChangeSysTime
 
         public static void StartService()
         {
-            System.Timers.Timer timer = new Timer(1000);
+            System.Timers.Timer timer = new Timer(200);
             timer.Elapsed += TimerOnElapsed;
             timer.Start();
         }
 
-        private static int timeCount = 0;
-
         private static void TimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
         {
-            timeCount++;
             DateTime n = DateTime.Now;
 
             if (n.Year == 2008)
             {
-                // 服务至少运行较长一段时间，因为在系统开机之后，可能自动对时，将时间改回原来的值
-                if (timeCount > 100)
-                {
-                    if (CanStop != null)
-                    {
-                        CanStop();
-                    }
-                }
                 return;
             }
 
             // 将年份改为2008年                  
             string dtstr = string.Format("2008-{0}-{1} {2}:{3}:{4}", n.Month, n.Day, n.Hour, n.Minute, n.Second);
-
             SysTimePro.SetLocalTimeByStr(dtstr);
 
         }
--- ChangeSysTimeApp/Program.cs
+++ ChangeSysTimeApp/Program.cs
@@ -1,19 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 
 namespace ChangeSysTimeApp
 {
-    class Program
+    internal class Program
     {
         /*
          * 手动更改系统时间
          */
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            DateTime now = DateTime.Now;
-            SysTimePro.SetTimeByCmd("2008",now.Month.ToString(),now.Day.ToString());
+        
+                /** 
+                * 当前用户是管理员的时候，直接启动应用程序 
+                * 如果不是管理员，则使用启动对象启动程序，以确保使用管理员身份运行 
+                */
+
+                //获得当前登录的Windows用户标示 
+            System.Security.Principal.WindowsIdentity identity =
+                System.Security.Principal.WindowsIdentity.GetCurrent();
+ 
+            System.Security.Principal.WindowsPrincipal principal =
+                new System.Security.Principal.WindowsPrincipal(identity);
+
+                //判断当前登录用户是否为管理员 
+            if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
+            {
+                //如果是管理员，则直接运行 
+                ProcessMethod();
+            }
+            else
+            {
+                //创建启动对象 
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                //设置运行文件 
+                startInfo.FileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                //设置启动参数 
+                startInfo.Arguments = String.Join(" ", args);
+                //设置启动动作,确保以管理员身份运行 
+                startInfo.Verb = "runas";
+                //如果不是管理员，则启动UAC 
+                System.Diagnostics.Process.Start(startInfo);
+                //退出 
+                //System.Windows.Forms.Application.Exit();
+                
+            }
+         
         }
+
+        static void ProcessMethod()
+        {
+
+
+            int timespan = 20; // 20 毫秒
+            int count = 20*(1000/timespan); // 20 秒
+
+            Console.WriteLine("倒计时 " + count / (1000 / timespan) + " 秒；");
+
+            string currentYear = DateTime.Now.Year.ToString();
+
+            System.Timers.Timer timer = new Timer(timespan);
+            timer.Elapsed += (sender, args) =>
+            {
+                DateTime now = DateTime.Now;
+                if (now.Year != 2008)
+                {
+                    //SysTimePro.SetTimeByCmd("2008",now.Month.ToString(),now.Day.ToString());
+                    string timeStr = "2008" + now.ToString("-MM-dd HH:mm:ss");
+                    SysTimePro.SetLocalTimeByStr(timeStr);
+                }
+
+                Console.Write("-");
+                if (count%10 == 0)
+                {
+                    Console.WriteLine();
+                }
+
+                if (--count == 0)
+                {
+                    timer.Stop();
+                    string timeStr = currentYear + now.ToString("-MM-dd HH:mm:ss");
+                    SysTimePro.SetLocalTimeByStr(timeStr);
+                    System.Environment.Exit(1);
+                }
+                else
+                {
+                    if (count%(1000/timespan) == 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("倒计时 " + count/(1000/timespan) + " 秒；");
+                    }
+                }
+            };
+            timer.Start();
+
+            while (true)
+            {
+                
+            }
+
+        }
+
+
+
     }
 }
